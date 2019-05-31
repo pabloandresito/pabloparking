5d600f7f9cf8e4d52d883db065b9798e2f3823fd
pabloandresito <pabloandresito@gmail.com>
2019-05-31 11:00:18 -0500

    Commit Inicial

---
+++ src/main/java/com/example/demo/PabloparkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PabloparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PabloparkingApplication.class, args);
+	}
+
+}
