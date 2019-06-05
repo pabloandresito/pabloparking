d743960bcde127f85164281087cc7e1572643559
pabloandresito <pabloandresito@gmail.com>
2019-06-05 07:53:56 -0500

    INADN-4128 Archivo Inicial de Jenkis y Sonar

--- src/main/java/com/ceiba/pabloparking/PabloparkingApplication.java
+++ src/main/java/com/ceiba/pabloparking/PabloparkingApplication.java
@@ -1,6 +1,5 @@
 package com.ceiba.pabloparking;
 
-import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -8,11 +7,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.ceiba.pabloparking.aplicacion.builder.MotoBuilder;
 import com.ceiba.pabloparking.aplicacion.builder.ParqueaderoBuilder;
 import com.ceiba.pabloparking.dominio.EstadoVehiculo;
+import com.ceiba.pabloparking.dominio.Moto;
 import com.ceiba.pabloparking.dominio.Parqueadero;
 import com.ceiba.pabloparking.dominio.TipoVehiculo;
+import com.ceiba.pabloparking.infraestructura.persistencia.dao.MotoDao;
 import com.ceiba.pabloparking.infraestructura.persistencia.dao.ParqueaderoDao;
+import com.ceiba.pabloparking.infraestructura.persistencia.entidad.MotoEntidad;
 import com.ceiba.pabloparking.infraestructura.persistencia.entidad.ParqueaderoEntidad;
 
 @SpringBootApplication
@@ -24,35 +27,35 @@ public class PabloparkingApplication {
 		SpringApplication.run(PabloparkingApplication.class, args);
 	}
 	
-//	@Bean
-//	public CommandLineRunner demo(MotoDao motoDao) {
-//		return (args) -> {
-//			// save a couple of customers
-//			motoDao.save(MotoBuilder.convertirAEntity(new Moto("MSP555", 300)));
-//			motoDao.save(MotoBuilder.convertirAEntity(new Moto("MSP666", 200)));
-//			motoDao.save(MotoBuilder.convertirAEntity(new Moto("MSP777", 100)));
-//			
-//			// fetch all customers
-//			log.info("Motos found with findAll():");
-//			log.info("-------------------------------");
-//			for (MotoEntidad motoEntidad : motoDao.findAll()) {
-//				log.info(motoEntidad.getPlaca());
-//			}
-//			log.info("");
-//
-//			// fetch an individual customer by ID
-//			MotoEntidad motoEntidadConsulta = motoDao.findByPlaca("MSP707");
-//			
-//			if(motoEntidadConsulta != null) {
-//				log.info("Moto found with findByPlaca:");
-//				log.info("--------------------------------");
-//				log.info(motoEntidadConsulta.getPlaca());
-//				log.info("");
-//			} else {
-//				log.info("Moto not found with findByPlaca");
-//			}			
-//		};
-//	}
+	@Bean
+	public CommandLineRunner demo(MotoDao motoDao) {
+		return (args) -> {
+			// save a couple of customers
+			motoDao.save(MotoBuilder.convertirAEntity(new Moto("MSP555", 300)));
+			motoDao.save(MotoBuilder.convertirAEntity(new Moto("MSP666", 200)));
+			motoDao.save(MotoBuilder.convertirAEntity(new Moto("MSP777", 100)));
+			
+			// fetch all customers
+			log.info("Motos found with findAll():");
+			log.info("-------------------------------");
+			for (MotoEntidad motoEntidad : motoDao.findAll()) {
+				log.info(motoEntidad.getPlaca());
+			}
+			log.info("");
+
+			// fetch an individual customer by ID
+			MotoEntidad motoEntidadConsulta = motoDao.findByPlaca("MSP707");
+			
+			if(motoEntidadConsulta != null) {
+				log.info("Moto found with findByPlaca:");
+				log.info("--------------------------------");
+				log.info(motoEntidadConsulta.getPlaca());
+				log.info("");
+			} else {
+				log.info("Moto not found with findByPlaca");
+			}			
+		};
+	}
 	
 	@Bean
 	public CommandLineRunner demoParqueadero(ParqueaderoDao parqueaderoDao) {
