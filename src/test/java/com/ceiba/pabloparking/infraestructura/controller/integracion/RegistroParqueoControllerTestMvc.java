4faa4cd877119893d938a4068fbaa9c859d3722f
pabloandresito <pabloandresito@gmail.com>
2019-06-13 10:18:43 -0500

    INADN-4132 Consumir servicios desde el Front End - Pruebas Mejoradas MockMvc a los controllers

--- src/test/java/com/ceiba/pabloparking/infraestructura/controller/integracion/RegistroParqueoControllerTestMvc.java
+++ src/test/java/com/ceiba/pabloparking/infraestructura/controller/integracion/RegistroParqueoControllerTestMvc.java
@@ -1,6 +1,7 @@
 package com.ceiba.pabloparking.infraestructura.controller.integracion;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -9,13 +10,17 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.ceiba.pabloparking.aplicacion.fabrica.FabricaRegistroParqueo;
 import com.ceiba.pabloparking.aplicacion.manejador.ManejadorVigilanteRegistrarVehiculo;
+import com.ceiba.pabloparking.dominio.TipoVehiculo;
 import com.ceiba.pabloparking.infraestructura.controller.RegistroParqueoController;
+import com.ceiba.pabloparking.infraestructura.controller.dto.RegistroParqueoDto;
 import com.ceiba.pabloparking.infraestructura.persistencia.dao.ConexionDBRegistroParqueo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(RegistroParqueoController.class)
@@ -23,6 +28,9 @@ public class RegistroParqueoControllerTestMvc {
 
     @Autowired
     private MockMvc mockMvc;
+    
+    @Autowired
+    private ObjectMapper objectMapper;
     
     @MockBean
     private ConexionDBRegistroParqueo conexionDBRegistroParqueo;
@@ -36,5 +44,19 @@ public class RegistroParqueoControllerTestMvc {
     @Test
     public void listVehiculosTestMvc() throws Exception {
         this.mockMvc.perform(get("/registro-parqueo/list-vehiculos")).andDo(print()).andExpect(status().isOk());
+    }
+    
+    @Test
+    public void ingresarTestMvc() throws Exception {
+    	
+    	RegistroParqueoDto registroParqueoDto = new RegistroParqueoDto();
+    	registroParqueoDto.setPlaca("GHB383");
+    	registroParqueoDto.setTipoVehiculo(TipoVehiculo.CARRO.getIdTipoVehiculo());
+    	
+        this.mockMvc.perform(post("/registro-parqueo/ingresar")
+        					.contentType(MediaType.APPLICATION_JSON_UTF8)
+        					.content(objectMapper.writeValueAsString(registroParqueoDto)) )
+        					.andDo(print())
+        					.andExpect(status().isCreated());
     }
 }
--- src/test/java/com/ceiba/pabloparking/infraestructura/controller/integracion/RetiroParqueoControllerTestMvc.java
+++ src/test/java/com/ceiba/pabloparking/infraestructura/controller/integracion/RetiroParqueoControllerTestMvc.java
@@ -1,6 +1,7 @@
 package com.ceiba.pabloparking.infraestructura.controller.integracion;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -9,13 +10,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.ceiba.pabloparking.aplicacion.fabrica.FabricaRegistroParqueo;
 import com.ceiba.pabloparking.aplicacion.manejador.ManejadorVigilanteRetirarVehiculo;
 import com.ceiba.pabloparking.infraestructura.controller.RetiroParqueoController;
+import com.ceiba.pabloparking.infraestructura.controller.dto.RegistroParqueoDto;
 import com.ceiba.pabloparking.infraestructura.persistencia.dao.ConexionDBRegistroParqueo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(RetiroParqueoController.class)
@@ -23,6 +27,9 @@ public class RetiroParqueoControllerTestMvc {
 
     @Autowired
     private MockMvc mockMvc;
+    
+    @Autowired
+    private ObjectMapper objectMapper;
     
     @MockBean
     private ConexionDBRegistroParqueo conexionDBRegistroParqueo;
@@ -36,5 +43,18 @@ public class RetiroParqueoControllerTestMvc {
     @Test
     public void listVehiculosRetiradosTestMvc() throws Exception {
         this.mockMvc.perform(get("/retiro-parqueo/list")).andDo(print()).andExpect(status().isOk());
+    }
+    
+    @Test
+    public void retirarTestMvc() throws Exception {
+    	
+    	RegistroParqueoDto registroParqueoDto = new RegistroParqueoDto();
+    	registroParqueoDto.setId(1l);
+    	
+        this.mockMvc.perform(post("/retiro-parqueo/retirar")
+        					.contentType(MediaType.APPLICATION_JSON_UTF8)
+        					.content(objectMapper.writeValueAsString(registroParqueoDto)) )
+        					.andDo(print())
+        					.andExpect(status().isOk());
     }
 }
